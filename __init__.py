4314732b3e97f765abf85ac3064f696f2989da8b
Rimvydas Naktinis <naktinis@gmail.com>
2012-04-24 11:38:53 +0300

    Moved auth code to a separate file.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from oauth2 import OAuth2
 
 
 class BasecampError(Exception):
@@ -153,30 +152,3 @@ class Events(Endpoint):
             if len(events) < self.PAGE_SIZE:
                 break
             page += 1
-
-
-class Auth(object):
-
-    AUTH_URL_BASE = 'https://launchpad.37signals.com/authorization/'
-    AUTH_URL_NEW = 'new'
-    AUTH_URL_TOKEN = 'token'
-
-    def __init__(self, client_id, client_secret, redirect_url):
-        self.oauth2 = OAuth2(client_id,
-                client_secret,
-                self.AUTH_URL_BASE,
-                redirect_url,
-                self.AUTH_URL_NEW,
-                self.AUTH_URL_TOKEN)
-
-    def authorize_url(self):
-        """Return an authorization url that opens a dialog for a user.
-        """
-        return self.oauth2.authorize_url(type='web_server')
-
-    def access_token(self, code):
-        """Return a dictionary with access token, expiration time, and
-        refresh token.
-        """
-        response = self.oauth2.get_token(code, type='web_server')
-        return response
---
+++ auth.py
@@ -0,0 +1,28 @@
+from oauth2 import OAuth2
+
+
+class Auth(object):
+
+    AUTH_URL_BASE = 'https://launchpad.37signals.com/authorization/'
+    AUTH_URL_NEW = 'new'
+    AUTH_URL_TOKEN = 'token'
+
+    def __init__(self, client_id, client_secret, redirect_url):
+        self.oauth2 = OAuth2(client_id,
+                client_secret,
+                self.AUTH_URL_BASE,
+                redirect_url,
+                self.AUTH_URL_NEW,
+                self.AUTH_URL_TOKEN)
+
+    def authorize_url(self):
+        """Return an authorization url that opens a dialog for a user.
+        """
+        return self.oauth2.authorize_url(type='web_server')
+
+    def access_token(self, code):
+        """Return a dictionary with access token, expiration time, and
+        refresh token.
+        """
+        response = self.oauth2.get_token(code, type='web_server')
+        return response
