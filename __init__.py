68e55bb039beefb904c7ff556b46860235d89f28
Rimvydas Naktinis <naktinis@gmail.com>
2012-04-24 12:40:53 +0300

    Added messages endpoint.

--- __init__.py
+++ __init__.py
@@ -23,10 +23,6 @@ class Client(object):
                          'Authorization': 'Bearer %s' % access_token,
                          'Content-Type': 'application/json; charset=utf-8'})
 
-    def qualified_url(self, url):
-        assert self.account_id is not None, "No account id given!"
-        return '%s/%s.json' % (self.BASE_URL % self.account_id, url)
-
     def accounts(self):
         url = '%s/authorization.json' % self.LAUNCHPAD_URL
         return self.session.get(url).content
@@ -37,15 +33,20 @@ class Endpoint(object):
     def __init__(self, client):
         self.client = client
 
+    def qualified_url(self, url):
+        assert self.client.account_id is not None, \
+                "Pass an account id to the Client to make this request!"
+        return '%s/%s.json' % (self.client.BASE_URL % self.client.account_id, url)
+
     def _get(self, url, params={}):
-        resp = self.client.session.get(self.client.qualified_url(url),
+        resp = self.client.session.get(self.qualified_url(url),
                 params=params)
         if resp.status_code != 200:
             raise BasecampError(resp.status_code)
         return json.loads(resp.content)
 
     def _post(self, url, data={}, expect=201):
-        resp = self.client.session.post(self.client.qualified_url(url),
+        resp = self.client.session.post(self.qualified_url(url),
                 json.dumps(data))
         if resp.status_code != expect:
             raise BasecampError(resp.status_code)
@@ -53,16 +54,32 @@ class Endpoint(object):
             return json.loads(resp.content)
 
     def _put(self, url, data={}):
-        resp = self.client.session.put(self.client.qualified_url(url),
+        resp = self.client.session.put(self.qualified_url(url),
                 json.dumps(data))
         if resp.status_code != 200:
             raise BasecampError(resp.status_code)
         return json.loads(resp.content)
 
     def _delete(self, url):
-        resp = self.client.session.delete(self.client.qualified_url(url))
+        resp = self.client.session.delete(self.qualified_url(url))
         if resp.status_code != 204:
             raise BasecampError(resp.status_code)
+
+
+class ProjectEndpoint(Endpoint):
+    """A thing that can only be inside a project.
+    E.g. messages, todos, comments etc.
+    """
+
+    BASE_URL = 'projects'
+
+    def __init__(self, client, project_id):
+        super(ProjectEndpoint, self).__init__(client)
+        self.project_id = project_id
+
+    def qualified_url(self, url):
+        return super(ProjectEndpoint, self).qualified_url('%s/%s/%s' % (
+            Projects.BASE_URL, self.project_id, url))
 
 
 class Projects(Endpoint):
@@ -154,21 +171,42 @@ class Events(Endpoint):
             page += 1
 
 
-class Topics(Endpoint):
+class Topics(ProjectEndpoint):
 
     BASE_URL = 'topics'
     PAGE_SIZE = 50
 
-    def list(self, project_id):
+    def list(self):
         """List all topics available in a given project.
         """
-        url = '%s/%s/%s' % (Projects.BASE_URL, project_id, self.BASE_URL)
-
         page = 1
         while True:
-            topics = self._get(url, params={'page': page})
+            topics = self._get(self.BASE_URL, params={'page': page})
             for topic in topics:
                 yield topic
             if len(topics) < self.PAGE_SIZE:
                 break
             page += 1
+
+
+class Messages(ProjectEndpoint):
+
+    BASE_URL = 'messages'
+
+    def get(self, message_id):
+        return self._get('%s/%s' % (self.BASE_URL, message_id))
+
+    def post(self, subject, content=None, attachments=None):
+        return self._post(self.BASE_URL,
+                {'subject': subject,
+                 'content': content,
+                 'attachments': attachments})
+
+    def update(self, message_id, subject, content=None, attachments=None):
+        return self._put('%s/%s' % (self.BASE_URL, message_id),
+                {'subject': subject,
+                 'content': content,
+                 'attachments': attachments})
+
+    def delete(self, message_id):
+        return self._delete('%s/%s' % (self.BASE_URL, message_id))
