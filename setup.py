3479c374fe93d1544cdd5a9d73a09167f309580e
Rimvydas Naktinis <naktinis@gmail.com>
2012-05-10 21:56:43 +0300

    Fixed documentation file extension in manifest file.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ required = ['requests>=0.11.2',
 
 setup(
     name='basecampx',
-    version='0.1.3',
+    version='0.1.4',
     author='Rimvydas Naktinis',
     author_email='naktinis@gmail.com',
     description=('Wrapper for Basecamp Next API.'),
